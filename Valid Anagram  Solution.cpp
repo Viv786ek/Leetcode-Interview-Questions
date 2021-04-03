41a72dd29005c95194d470fa1823edb9af1ad667
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-04-03 13:54:42 +0530

    Update Valid Anagram  Solution.cpp

--- Valid Anagram  Solution.cpp
+++ Valid Anagram  Solution.cpp
@@ -1,3 +1,4 @@
+//1st
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -5,6 +6,28 @@ public:
         sort(t.begin(), t.end()); 
         return s == t; 
         
+        
+    }
+};
+
+//2nd
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+        return false;
+    }
+    int counter[26]={0};
+    for (int i = 0; i < s.length(); i++) {
+        counter[s[i] - 'a']++;
+        counter[t[i] - 'a']--;
+    }
+    for (int count : counter) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
         
     }
 };
