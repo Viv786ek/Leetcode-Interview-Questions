bb106ed9caa863ce7498af6f273e2b5fe4e321f2
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-05-19 14:41:39 +0530

    Create count and say.cpp

---
+++ count and say.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        if(n==2) return "11";
+        string s="11";
+        for(int i=3;i<=n;i++){
+            s = s+'$';
+            string temp ="";
+            int count=1;
+            for(int j=1;j<s.size();j++){
+                if(s[j] != s[j-1]){
+                    temp += to_string(count);
+                    temp += s[j-1];
+                    count =1;
+                }
+                else count++;
+            }
+            s = temp;
+        }
+        return s;
+    }
+};
