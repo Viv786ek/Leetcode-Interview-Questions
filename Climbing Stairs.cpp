f90d73412b09a52e60c5ac9dbc9a91380c666a89
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-06-02 23:45:20 +0530

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -4,6 +4,27 @@ public:
         if(n==1){
             return 1;
         }
+        int first =1;
+        int second =2;
+        for(int i=3;i<=n;i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+        
+    }
+};
+
+
+
+// 2nd
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
         int dp[n+1];
         dp[0]=0;
         dp[1]=1;
