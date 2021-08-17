34cfcd7b9665d73fefeec8e5542652cf0a34c0ae
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-08-17 12:16:18 +0530

    Create Implement strStr().cpp
    https://leetcode.com/problems/implement-strstr/

---
+++ Implement strStr().cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/implement-strstr/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle=="")
+            return 0;
+        if(haystack.find(needle) != string::npos){
+            return haystack.find(needle);
+        }
+        return -1;
+        
+    }
+};
+
